c04601a4009dea11767565a1de6d272526b05610
Cintia Arce <cintiaayelenarce@gmail.com>
2022-10-31 17:23:27 -0300

    path & props

--- pages/edit/[id]/index.js
+++ pages/edit/[id]/index.js
@@ -1,5 +1,7 @@
 import React, { useState, useRef } from "react";
 import { useRouter } from "next/router";
+import Inputs from "../../../components/Inputs";
+import Buttons from "../../../components/Buttons";
 
 function EditItem() {
   const router = useRouter();
@@ -24,121 +26,74 @@ function EditItem() {
   };
 
   return (
-    <div>
+    <div className="w-full">
+      <h1 className="text-center text-4xl font-semibold">New Invoice</h1>
       <div>
         <div>
-          <h3>Edit INVOICE_ID</h3>
+          <p className="font-medium text-xl">Bill from </p>
+          <Inputs name={"senderStreet"} text="Streer Address" />
+          <div className="flex justify-between">
+            <Inputs text={"City"} name="senderCity" />
+            <Inputs text={"Postal Code"} name="senderPostalCode" />
+            <Inputs text={"Country"} name="senderCountry" />
+          </div>
+        </div>
+        <div>
+          <p className="font-medium text-xl">Bill to </p>
+          <Inputs text={"Client Name"} name="clientName" />
+          <Inputs text={"Client Email"} name="clientEmail" />
+          <Inputs text={"Client Street Address"} name="clientStreet" />
+          <div className="flex justify-between">
+            <Inputs text={"Client City"} name="clientCity" />
+            <Inputs text={"Client Postal Code"} name="clientPostalCode" />
+            <Inputs text={"Client Country"} name="clientCountry" />
+          </div>
+          <div className="flex justify-between">
+            <Inputs text={"Invoice Date"} name="createdAt" type="date" />
+            <Inputs text={"Payment Terms"} name="paymentTerms" />
+          </div>
+          <Inputs text={"Project Description"} name="description" />
         </div>
         <div>
           <div>
-            <p>Bill from </p>
-            <div>
-              <p>Street Address</p>
-              <input type="text" />
-            </div>
-            <div>
-              <div>
-                <p>City</p>
-                <input type="text" />
-              </div>
-              <div>
-                <p>Postal Code</p>
-                <input type="text" />
-              </div>
-              <div>
-                <p>Country</p>
-                <input type="text" />
-              </div>
-            </div>
-          </div>
-          <div>
-            <p>Bill to </p>
-            <div>
-              <p>Client Name</p>
-              <input type="text" />
-            </div>
-            <div>
-              <p>Client Email</p>
-              <input type="email" />
-            </div>
-            <div>
-              <p>Street Address</p>
-              <input type="text" />
-            </div>
-            <div>
-              <div>
-                <p>City</p>
-                <input type="text" />
-              </div>
-              <div>
-                <p>Postal Code</p>
-                <input type="text" />
-              </div>
-              <div>
-                <p>Country</p>
-                <input type="text" />
-              </div>
-            </div>
-            <div>
-              <div>
-                <p>Invoice Date</p>
-                <input type="date" />
-              </div>
-              <div>
-                <p>Payment Terms</p>
-                <input type="text" />
-              </div>
-            </div>
-            <div>
-              <p>Project Description</p>
-              <input type="text" />
-            </div>
-          </div>
-          <div>
-            <div>
-              <h3>Item List</h3>
+            <h3 className="mb-5">Item List</h3>
+            <div className="overflow-auto">
               {items?.map((item, i) => (
-                <div key={i}>
+                <div key={i} className="flex justify-between mb-3">
+                  <Inputs text={"Item Name"} name="name" />
+                  <Inputs text={"Quantity"} name="quantity" type={"number"} />
+                  <Inputs text={"Price"} name="price" type={"number"} />
                   <div>
-                    <div>
-                      <p>Item Name</p>
-                      <input
-                        type="text"
-                        name="name"
-                        onClick={(e) => handleChange(e, i)}
-                      />
-                    </div>
-                    <div>
-                      <p>Qty</p>
-                      <input
-                        type="number"
-                        name="quantity"
-                        onClick={(e) => handleChange(e, i)}
-                      />
-                    </div>
-                    <div>
-                      <p>Price</p>
-                      <input
-                        type="number"
-                        name="price"
-                        onClick={(e) => handleChange(e, i)}
-                      />
-                    </div>
-                    <div>
-                      <p>Total</p>
-                      <h4>${item.total}</h4>
-                    </div>
-                    <button onClick={() => handleDeleteItem(i)}>Delete</button>
+                    <p>Total</p>
+                    <h4>${item.total}</h4>
                   </div>
+                  <Buttons text={"Delete"} handle={() => handleDeleteItem(i)} />
                 </div>
               ))}
             </div>
-            <button onClick={addItem}>Add New Item</button>
-            <div>
-              <div>
-                <button onClick={`/invoice/id`}>Cancel</button>
-                <button>Save Changes</button>
-              </div>
+          </div>
+          <Buttons
+            styles={"w-full h-10"}
+            text={"Add New Item"}
+            handle={addItem}
+          />
+          <div className="flex justify-between my-5">
+            <Buttons
+              handle={() => router.push("/")}
+              text="Discard"
+              styles=" w-100 h-10"
+            />
+            <div className="flex justify-around">
+              <Buttons
+                text={"Save as Draft"}
+                handle={() => handleSubmit("draft")}
+                color="transparent w-100 h-10 mx-2"
+              />
+              <Buttons
+                text={"Send & Save"}
+                handle={() => handleSubmit("pending")}
+                styles=" w-100 h-10"
+              />
             </div>
           </div>
         </div>
@@ -148,3 +103,42 @@ function EditItem() {
 }
 
 export default EditItem;
+
+export async function getStaticPaths() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://mongoDB:mongoDB123@cluster0.deg35s0.mongodb.net/invoices?retryWrites=true&w=majority",
+    { useNewUrlParser: true }
+  );
+  const db = client.db().collection("allInvoices");
+  const invoices = await db.find({}, { _id: 1 }).toArray();
+
+  return {
+    fallback: "blocking",
+    paths: invoices.map((i) => ({
+      params: {
+        invoiceId: (i._id = String(i._id)),
+      },
+    })),
+  };
+}
+
+export async function getStaticProps(context) {
+  const { invoiceId } = context.params;
+
+  const client = await MongoClient.connect(
+    "mongodb+srv://mongoDB:mongoDB123@cluster0.deg35s0.mongodb.net/invoices?retryWrites=true&w=majority",
+    { useNewUrlParser: true }
+  );
+  const db = client.db().collection("allInvoices");
+  const invoice = await db.findOne({ _id: ObjectId(invoiceId) });
+
+  return {
+    props: {
+      data: {
+        id: (invoice._id = String(invoice._id)),
+        ...invoice,
+      },
+    },
+    revalidate: 1,
+  };
+}
