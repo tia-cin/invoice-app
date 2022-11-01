1f4c4015f24a5df394c53f67cb75a25f60d95756
Cintia Arce <cintiaayelenarce@gmail.com>
2022-11-01 15:44:41 -0300

    delete navbar

--- components/Layout.js
+++ components/Layout.js
@@ -1,14 +1,12 @@
 import React from "react";
-import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
 
 const Layout = (props) => {
   return (
     <main className="flex h-screen">
       <Sidebar />
-      <div className="w-full h-screen overflow-auto bg-[#0A0E1A] cursor-default text-[#f9f9f9]">
-        <Navbar />
-        <div className="py-2 px-10">{props.children}</div>
+      <div className="w-full h-screen overflow-auto bg-[#0A0E1A] cursor-default text-[#f9f9f9] py-2 px-10">
+        {props.children}
       </div>
     </main>
   );
--- components/Sidebar.js
+++ components/Sidebar.js
@@ -10,12 +10,9 @@ const Sidebar = () => {
     <div className="h-screen flex flex-col justify-between w-300 bg-liliac">
       <div>
         <div className="flex p-5">
-          <img
-            src="https://cdn-icons-png.flaticon.com/512/87/87388.png?w=740&t=st=1666964877~exp=1666965477~hmac=64e6534298801bcfe121b4962db027e213d07b774241a7a57829fe2b9a21da76"
-            alt="invoice-logo"
-            className="w-10 h-10 bg-white rounded-full p-1"
-          />
-          <p className="text-2xl mx-2 mt-1 text-black font-semibold">Invoice</p>
+          <p className="text-3xl mx-2 mt-1 text-black font-semibold">
+            Invoices
+          </p>
         </div>
         <div className="ml-5">
           {links.map((item, i) => (
@@ -30,7 +27,6 @@ const Sidebar = () => {
           ))}
         </div>
       </div>
-      <Buttons styles="w-200 h-10 bg-third-dark m-5" text={"Logout"} />
     </div>
   );
 };
